cd45dac7bee4338f3ddf61b024301b63a70a7801
Little-Bear-04 <142242630+Little-Bear-04@users.noreply.github.com>
2023-10-28 08:55:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const knex = require('knex');
 
 // const register = require('./Controllers/register');
 import { handleRegister } from './Controllers/register';
-const signin = require('./Controllers/signin.js');
-const profile = require('./Controllers/profile.js');
-const image = require('./Controllers/image.js');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
 
 const db = knex({
     client: 'pg',
@@ -39,6 +39,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfilGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
 })
